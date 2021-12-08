fda92780f3f8331356cca070b49acb7dc7030dea
Nicolas Theye <nicolas.theye@gmail.com>
2021-12-08 18:42:28 +0100

    rename variables + methods

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const listTest = [0, 1, 240, 21, 10, 23, 31];
 
-const displayGenre = (list) =>{
+const displayGender = (list) =>{
     list.forEach( n => {
         if (n % 15 === 0) console.log(" GarçonFille ");
         else if (n % 3 === 0) console.log(" Garçon ");
@@ -8,49 +8,49 @@ const displayGenre = (list) =>{
         else console.log(n);
     });
 }
-displayGenre(listTest);
+displayGender(listTest);
 
 
-const dateListTest = [new Date(2021, 3, 28, 18, 54), new Date(2021, 7, 4, 7, 5), new Date(2018, 5, 2, 3, 4)];
+const dateList = [new Date(2021, 3, 28, 18, 54), new Date(2021, 7, 4, 7, 5), new Date(2018, 5, 2, 3, 4)];
 
-const lastMonthDay = (n) => {
-    const date = new Date(n).setDate(0);
-    const dateToStr = new Date(date).toLocaleDateString();
-    return dateToStr;
+const showLastDayOfLastMonth = (date) => {
+    const lastDayOfLastMonth = new Date(date).setDate(0);
+    const lastDayOfLastMonthToStr = new Date(lastDayOfLastMonth).toLocaleDateString();
+    return lastDayOfLastMonthToStr;
 };
 
-const dateZeroed = (n) => {
-    const date = new Date(n);
-    date.setHours(0);
-    date.setMinutes(0);
-    return date;
+const setHoursAndMinutesToZero = (date) => {
+    const dateNotReset = new Date(date);
+    dateNotReset.setHours(0);
+    const dateWithResetHoursMinutes = dateNotReset.setMinutes(0);
+    return dateWithResetHoursMinutes;
 };
 
-const dateFormatted = (n) => {
-    const date = new Date(n);
-    const dateWithFormat = date.toLocaleDateString() + ' ' +
-                            [date.getHours().toString().padStart(2, '0'),
-                            date.getMinutes().toString().padStart(2, '0')].join(':');
-    return dateWithFormat;
+const formatDate = (date) => {
+    const dateNoFormat = new Date(date);
+    const dateFormatted = dateNoFormat.toLocaleDateString() + ' ' +
+                            [dateNoFormat.getHours().toString().padStart(2, '0'),
+                            dateNoFormat.getMinutes().toString().padStart(2, '0')].join(':');
+    return dateFormatted;
 };
 
-const weekNumber = (n) => {
-    const date = new Date(n);
-    const firstJan = new Date(date.getFullYear(), 0, 1);
-    const numberOfDays = Math.floor((date - firstJan) / (24 * 60 * 60 * 1000));
-    const numberOfTheWeek = Math.ceil((date.getDay() + 1 + numberOfDays) / 7);
+const showWeekNumber = (date) => {
+    const dateDay = new Date(date);
+    const firstJanuary = new Date(dateDay.getFullYear(), 0, 1);
+    const numberOfDaysInBetween = Math.floor((dateDay - firstJanuary) / (24 * 60 * 60 * 1000));
+    const numberOfTheWeek = Math.ceil((dateDay.getDay() + 1 + numberOfDaysInBetween) / 7);
     return numberOfTheWeek;
 };
 
-const result = (dateList) => {
+const showResults = (dateList) => {
     return (dateList.map((date) => (
         {
-            lastMonthDay: lastMonthDay(date),
-            dateZeroed: dateFormatted(dateZeroed(date)),
-            dateFormatted: dateFormatted(date),
-            weekNumber: weekNumber(date)
+            lastDayOfLastMonth: showLastDayOfLastMonth(date),
+            dateZeroed: formatDate(setHoursAndMinutesToZero(date)),
+            dateFormatted: formatDate(date),
+            weekNumber: showWeekNumber(date)
         }
     )));
 };
 
-console.log(result(dateListTest));
+console.log(showResults(dateList));
